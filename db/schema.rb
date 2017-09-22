e54388dcb4586857d4551a9781d56368376eaf8c
“harishpavan” <harishpavant@gmail.comgit config --global user.email harishpavant@gmail.com>
2017-09-22 12:47:22 -0400

    add association user and article

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920212119) do
+ActiveRecord::Schema.define(version: 20170922150931) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
