abf6a5d6e91ca21d6d74e6cf613ce97391b2e455
“harishpavan” <harishpavant@gmail.comgit config --global user.email harishpavant@gmail.com>
2017-09-23 20:30:00 -0400

    password authenticationn activated

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'jquery-ui-rails', '~> 5.0.5'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.2'
 # Use sqlite3 as the database for Active Record
+gem 'bcrypt', '~> 3.1.7'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170922150931) do
+ActiveRecord::Schema.define(version: 20170924002020) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20170922150931) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
